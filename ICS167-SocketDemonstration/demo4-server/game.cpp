0a990542e9b39bcd1ae1d87c2a8cabcbaf1d1c82
Tyler Gemora <tgemora@uci.edu>
2018-02-08 15:10:59 -0800

    New canvas parameters

--- ICS167-SocketDemonstration/demo4-server/game.cpp
+++ ICS167-SocketDemonstration/demo4-server/game.cpp
@@ -1,9 +1,9 @@
 #define _USE_MATH_DEFINES
 
-#define X_BOUNDS 500
-#define Y_BOUNDS 500
-#define PADDLE_OFFSET 20
-#define PADDLE_WIDTH 20
+#define X_BOUNDS 550
+#define Y_BOUNDS 550
+#define PADDLE_OFFSET 10
+#define PADDLE_WIDTH 120
 
 #include <map>
 #include <string>
